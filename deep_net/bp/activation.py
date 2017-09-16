a1cb03c258b75583a0650597e1dd27817e842950
fishedee <306766045@qq.com>
2017-09-17 00:12:42 +0800

    加入sigmoid的激活函数

--- deep_net/bp/activation.py
+++ deep_net/bp/activation.py
@@ -1,17 +1,25 @@
+import numpy as np
 class Activation:
 	def __init__(self,type,input_shape=None):
-		pass
+		self._type = type
+		self._input_shape = input_shape
 	def set_input_shape(self,input_shape):
-		pass
+		self._input_shape = input_shape
 	def get_output_shape(self):
-		pass
+		return self._input_shape
 	def get_init_weight(self):
-		pass
+		return np.array([])
 	def get_init_grade(self):
-		pass
+		return np.array([])
 	def get_output(self,weight,inData):
-		pass
+		if self._type == "sigmoid":
+			return 1.0/(1.0+np.exp(-inData))
+		else:
+			raise Exception("Unknown Activation Type: %s"%(self._type))
 	def get_loss(self,nextLoss,weight):
-		pass
+		if self._type == "sigmoid":
+			return np.exp(-nextLoss)/((1+np.exp(-nextLoss))**2)
+		else:
+			raise Exception("Unknown Activation Type: %s"%(self._type))
 	def get_grade(self,nextLoss,inData):
-		pass
+		return np.array([])
